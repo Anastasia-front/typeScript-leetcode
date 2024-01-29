ff755e8b0da39db90e6ecd27a6ef310adbad0bce
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-01-29 19:33:22 +0200

    add task haystack-needle

---
+++ easy/haystack-needle/haystack-needle.js
@@ -0,0 +1,24 @@
+"use strict";
+function strStr(haystack, needle) {
+    const lengthHaystack = haystack.length;
+    const lengthNeedle = needle.length;
+    const difference = lengthHaystack - lengthNeedle;
+    if (lengthNeedle === 0) {
+        return -1;
+    }
+    for (let index = 0; index < difference + 1; index++) {
+        if (haystack.slice(index, index + lengthNeedle) === needle) {
+            return index;
+        }
+    }
+    return -1;
+}
+// Runtime 60 ms / Memory 51.79 mb
+const res = strStr("sadbutsad", "sad");
+console.log(res);
+const res1 = strStr("leetcode", "leeto");
+console.log(res1);
+const res2 = strStr("hello", "ll");
+console.log(res2);
+const res3 = strStr("mississippi", "issip");
+console.log(res3);
