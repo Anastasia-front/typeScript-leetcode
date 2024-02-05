c73fbc1b46b77717ec32ef9b7b346b12d377c65a
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-02-05 18:30:56 +0200

    add task search-insert-position

---
+++ easy/earch-insert-position/search-insert-position.js
@@ -0,0 +1,20 @@
+"use strict";
+function searchInsert(numbers, target) {
+    if (target > numbers[numbers.length - 1]) {
+        return numbers.length;
+    }
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (target <= element) {
+            return i;
+        }
+        else {
+            continue;
+        }
+    }
+    return -1;
+}
+// Runtime 59 ms / Memory 51.81 mb
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
