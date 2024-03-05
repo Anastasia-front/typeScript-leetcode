a7b7919f4ce4d062f312765a2d29b0c2e1dc9f2b
Anastasia-front <palitsanastasia3.ap@gmail.com>
2024-03-05 15:26:57 +0200

    create climbing-stairs

---
+++ easy/climbing-stairs/climbing-stairs.js
@@ -0,0 +1,26 @@
+"use strict";
+function climbStairs(n) {
+    if (n === 0) {
+        return 0;
+    }
+    else if (n === 1) {
+        return 1;
+    }
+    else if (n === 2) {
+        return 2;
+    }
+    else {
+        let prev1 = 1;
+        let prev2 = 2;
+        for (let index = 3; index <= n; index++) {
+            let current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        return prev2;
+    }
+}
+;
+// Runtime 56 ms / Memory 50.20 mb
+console.log(climbStairs(6));
+console.log(climbStairs(18));
